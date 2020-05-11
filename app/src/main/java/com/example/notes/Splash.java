6cec6c670c1f8625fd9b8a22a447149d7ef9e6c5
awsmayush <43489922+awsmayush@users.noreply.github.com>
2020-05-11 18:52:43 +0530

    notes

---
+++ app/src/main/java/com/example/notes/Splash.java
@@ -0,0 +1,25 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    public  static int Spalsh_Time_out=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },Spalsh_Time_out);
+    }
+}
